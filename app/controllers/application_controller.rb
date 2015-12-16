6b1a816d53d903f679234a5c4f35a77f72f0eb7b
DuarteDuarte <dd3m@hotmail.com>
2015-12-16 11:53:16 +0000

    added user log in authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  #before_action :devise_configure_permitted_parameters, if: :devise_controller?
+
+  
+
 end
