f82d267f7280545203e2cbad3a16955232229c74
DuarteDuarte <dd3m@hotmail.com>
2015-12-16 13:23:57 +0000

    fixed has_many relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :ideas
-
+	has_many :participants
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
