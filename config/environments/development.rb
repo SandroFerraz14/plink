0fbd6149eb336845890c712f8fdbf5ef8d6ba6f3
Sandro Ferraz <sandro.ferraz.mei@gmail.com>
2015-12-23 16:24:09 +0000

    fix some config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,20 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+   #These settings are for the sending out email for active admin and consequently the   devise mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = 
+  {
+
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'gmail.com', 
+    :authentication       => :plain,
+    :user_name            => 'peigrupouce15@gmail.com',
+    :password             => 'UCE15grupoPEI',
+    :enable_starttls_auto => true
+  }
 end
