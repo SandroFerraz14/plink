d6ea451f357b0a70706963722ce685581514021b
duarteduarte <dd3m@hotmail.com>
2015-12-23 16:07:01 +0000

    change anonymity from boolean to integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223122919) do
+ActiveRecord::Schema.define(version: 20151223154727) do
 
   create_table "ideas", force: :cascade do |t|
     t.integer  "user_id"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20151223122919) do
     t.datetime "updated_at",  null: false
     t.integer  "nideas"
     t.integer  "user_id"
-    t.boolean  "anonymity"
+    t.integer  "anonymity"
   end
 
   add_index "ideation_sessions", ["user_id"], name: "index_ideation_sessions_on_user_id"
