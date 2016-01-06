059e8f208f802abadb3915a985e56232f052b9df
Sandro Ferraz <sandro.ferraz.mei@gmail.com>
2016-01-06 19:39:31 +0000

    change path after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   #before_action :devise_configure_permitted_parameters, if: :devise_controller?
 	
   def after_sign_in_path_for(resource_or_scope)
-    home_path
+    ideation_sessions_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
