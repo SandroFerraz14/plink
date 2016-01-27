4d26ace23cfb876024bf07c8dad77cb0cc053325
Sandro Ferraz <sandro.ferraz.mei@gmail.com>
2016-01-27 18:05:51 +0000

    uniqueness name for thems

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,6 @@
 class Theme < ActiveRecord::Base
 	belongs_to :ideation_session
 	has_many :ideas
+
+	validates :name, presence: true, uniqueness: {scope: :ideation_session, case_sensitive: false}
 end
