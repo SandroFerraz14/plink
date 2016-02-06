22b9bd9632fe104538b51d91fbd4693699a5c5d4
duarteduarte <dd3m@hotmail.com>
2016-02-06 01:16:45 +0000

    fixed editing name

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,13 +40,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
+  #   devise_parameter_sanitizer.for(:sign_up) << :name
   # end
 
-  # If you have extra params to permit, append them to the sanitizer.
+  # # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
+  #   devise_parameter_sanitizer.for(:account_update) << :name
   # end
+
+  def sign_up_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation, :current_password)
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
